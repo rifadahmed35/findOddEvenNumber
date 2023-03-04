43e88c57fe8abfdf7aea06f66ce5cada30c15cb4
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-04 13:30:24 +0100

    find odd even number

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int i;
+        i=11;
+        if(i%2==0){
+            System.out.println("The number is even");
+        }else{
+            System.out.println("The number is odd");
+        }
+    }
+
+}
